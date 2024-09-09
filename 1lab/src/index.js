a7a509a9499499956dab1f8ee0fb3be620a622ba
GiproveR <127661085+GiproveR@users.noreply.github.com>
2024-09-09 09:43:53 +0800

    Update index.js

--- 1lab/src/index.js
+++ 1lab/src/index.js
@@ -1,7 +1,5 @@
 const date_now = new Date();
-console.log(date_now);
-console.log(date_now.getFullYear()+1);
+//console.log(date_now);
 const date_NEWYEAR = new Date(date_now.getFullYear()+1, 0, 1);
-console.log(date_NEWYEAR);
-
+//console.log(date_NEWYEAR);
 console.log((date_NEWYEAR-date_now) / (1000 * 60 * 60 * 24));
