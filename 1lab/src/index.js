fbe215c0fa3ba8be87a6fdb6e9e77b1f46bc3085
GiproveR <127661085+GiproveR@users.noreply.github.com>
2024-09-09 09:50:45 +0800

    Update index.js

--- 1lab/src/index.js
+++ 1lab/src/index.js
@@ -2,4 +2,5 @@ const date_now = new Date();
 //console.log(date_now);
 const date_NEWYEAR = new Date(date_now.getFullYear()+1, 0, 1);
 //console.log(date_NEWYEAR);
-console.log((date_NEWYEAR-date_now) / (1000 * 60 * 60 * 24));
+const days = (date_NEWYEAR-date_now) / (1000 * 60 * 60 * 24);
+console.log("До нового года осталось", days.toFixed(1), "дней");
