3e6d739979593d7efffce9d8fe4d307efbef01f3
GiproveR <yaroslav.vjukoff1@mail.ru>
2024-09-22 20:55:54 +0800

    Изменение иерархии папок и файлов

--- 1lab/src/index.js
+++
@@ -1,6 +0,0 @@
-const date_now = new Date();
-//console.log(date_now);
-const date_NEWYEAR = new Date(date_now.getFullYear()+1, 0, 1);
-//console.log(date_NEWYEAR);
-const days = (date_NEWYEAR-date_now) / (1000 * 60 * 60 * 24);
-console.log("До нового года осталось", days.toFixed(1), "дней");
